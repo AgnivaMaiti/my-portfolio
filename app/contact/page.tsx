decf979ef450e95b0f1a1bb9f34fa55c633c64c6
Agniva Maiti <122050817+AgnivaMaiti@users.noreply.github.com>
2024-12-08 15:55:52 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { Github, Mail, LinkedIn } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
   {
-    icon: <LinkedIn size={20} />,
+    icon: <Linkedin size={20} />,
     href: "https://www.linkedin.com/in/agniva-maiti/",
     label: "LinkedIn",
     handle: "agniva-maiti",
