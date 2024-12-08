b49a8dc0f2d639c8e4dc985807556a357c0b2355
Agniva Maiti <122050817+AgnivaMaiti@users.noreply.github.com>
2024-12-08 15:43:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "agniva.tech",
+    template: "%s | agniva.tech",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Co-founder of mynimalistic.tech and founder of Tatvani",
   openGraph: {
-    title: "chronark.com",
+    title: "agniva.tech",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "Co-founder of mynimalistic.tech and founder of Tatvani",
+    url: "https://agniva.tech",
+    siteName: "agniva.tech",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://agniva.tech/og.png",
         width: 1920,
         height: 1080,
       },
@@ -37,8 +37,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+
   twitter: {
-    title: "Chronark",
+    title: "Agniva",
     card: "summary_large_image",
   },
   icons: {
