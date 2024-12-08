4ff337ee53f52b3ecc2afa75db53a667728c1874
Agniva Maiti <122050817+AgnivaMaiti@users.noreply.github.com>
2024-12-08 15:43:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,14 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Book-lover, cloud-gazer, and coffee aficionado on a coding adventure. Currently a CSE junior at KIIT-DU, Bhubaneswar, India. Building {" "}
+          Book-lover, cloud-gazer, and coffee aficionado on a coding adventure. Building {" "}
           <Link
             target="_blank"
             href="https://www.mynimalistic.tech/"
             className="underline duration-500 hover:text-zinc-300"
           >
             Mynimalistic
-          </Link> to help empower people in the diogital world.
+          </Link> to help empower people in the digital world.
         </h2>
       </div>
     </div>
