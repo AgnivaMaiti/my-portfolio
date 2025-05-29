473bc80e00deddc00b0b5ab4fa33e4d5f0352a35
AgnivaMaiti <maitiagniva@gmail.com>
2025-05-30 02:59:57 +0530

    Updated homepage description

--- app/page.tsx
+++ app/page.tsx
@@ -35,15 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Book-lover, cloud-gazer, and coffee aficionado on a coding adventure. Building {" "}
-          <Link
-            target="_blank"
-            href="https://www.mynimalistic.tech/"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            Mynimalistic
-          </Link> to help empower people in the digital world.
-        </h2>
+          Book-lover, cloud-gazer, and coffee aficionado on a coding adventure. </h2>
       </div>
     </div>
   );
