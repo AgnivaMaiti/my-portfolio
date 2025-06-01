aadf9012b79af7aff2f6d7a048b3af7352e1f97d
AgnivaMaiti <maitiagniva@gmail.com>
2025-06-02 01:29:16 +0530

    bug fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -124,7 +124,7 @@ export default async function ProjectsPage() {
           </Card>
 
           <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-            {[top2, top3].filter(Boolean).map((project: Project, _: number, i: number) => (
+            {[top2, top3].filter(Boolean).map((project: Project) => (
               <Card key={project.slug}>
                 <Article project={project} views={views[project.slug] ?? 0} />
               </Card>
